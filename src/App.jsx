63b25d1a6c8e32a414a31a9757ca24cdb74d8871
Dunay Gudratli <dunay.gudretli@azercell.com>
2026-03-12 15:01:36 +0400

    162: handle project deletion

--- src/App.jsx
+++ src/App.jsx
@@ -52,7 +52,18 @@ function App() {
     })
   }
 
+  function handleDeleteProject() {
+    setProjectsState(prevState => {
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+        projects: prevState.projects.filter(project => project.id !== prevState.selectedProjectId),
+      }
+    })
+  }
+
   let content = <SelectedProject
+    onDelete={handleDeleteProject}
     project={projectsState.projects.find(project => project.id === projectsState.selectedProjectId)}/>;
 
   if (projectsState.selectedProjectId === null) {
